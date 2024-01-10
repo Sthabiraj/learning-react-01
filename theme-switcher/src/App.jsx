8b53e6ea026107316d49e8d1588c16954ecf1d1f
Sthabiraj <birajshrestha2007@gmail.com>
2024-01-10 19:04:50 +0545

    Update code formatting and fix bug

---
+++ theme-switcher/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <div className="bg-white h-screen w-full flex justify-center items-center">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
